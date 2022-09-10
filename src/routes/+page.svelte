2a2bbd3019d8084b18a42cfb1c4cf47cf3dedc01
sdairs <sdairs@users.noreply.github.com>
2022-09-10 20:06:14 +0100

    front page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,30 @@
 <script lang="ts">
-	import { Button } from 'carbon-components-svelte';
 </script>
 
-<h1>Home page</h1>
+<h1>Welcome</h1>
+
+<h4>This is a demo application built for the <b>Getting Stuff Done With Data</b> conference.</h4>
+
+<h5>In the Application Menu (top right) you'll find links to different applications.</h5>
+
+<h5>
+	You can find the repo at
+	<a href="https://github.com/sdairs/gsd_delivery_demo">
+		https://github.com/sdairs/gsd_delivery_demo
+	</a>
+</h5>
+
+<style>
+	h1,
+	h4,
+	h5 {
+		text-align: center;
+	}
+	h4,
+	h5 {
+		padding-top: 1rem;
+	}
+	h4 > b {
+		font-weight: bold;
+	}
+</style>
